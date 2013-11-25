cf1310eecc535b6f021c1fcd9b111d86399e51b3
alled <alyosha.vol92@gmail.com>
2013-11-25 18:21:08 +0200

    info page added

---
+++ src/com/example/ski_4/Info.java
@@ -0,0 +1,11 @@
+package com.example.ski_4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alyosha
+ * Date: 25.11.13
+ * Time: 17:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class Info {
+}
